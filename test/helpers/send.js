1190d972ebb6832e7fca42412ea0f49fdc2453bf
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-07-25 21:40:33 +0100

    Add response callback

--- test/helpers/send.js
+++ test/helpers/send.js
@@ -1,19 +1,19 @@
-'use strict';
+'use strict'
 
-const request = require('request');
+const request = require('request')
 
-function send(type, data, context = '/') {
-  const opts = { url: `http://localhost:1234${context}` };
+function send(type, data, context = '/', cb) {
+    const opts = { url: `http://localhost:1234${context}` }
 
-  if (type === 'application/x-www-form-urlencoded') {
-    opts.form = data;
-  } else if (type === 'multipart/form-data') {
-    opts.formData = data;
-  } else if (type === 'application/json') {
-    opts.json = data;
-  }
+    if (type === 'application/x-www-form-urlencoded') {
+        opts.form = data
+    } else if (type === 'multipart/form-data') {
+        opts.formData = data
+    } else if (type === 'application/json') {
+        opts.json = data
+    }
 
-  request.post(opts);
+    request.post(opts, cb)
 }
 
-module.exports = send;
+module.exports = send
